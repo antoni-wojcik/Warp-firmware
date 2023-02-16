b2b89e6a2cfb8f706cff41cb1e1c3cd2d317764e
Antoni <antekwojcik2@gmail.com>
2023-02-16 20:29:08 +0000

    Load the SSD1331 drivers similarly to the other

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -105,10 +105,10 @@
 	volatile WarpI2CDeviceState			deviceMMA8451QState;
 #endif
 
-//#if (WARP_BUILD_ENABLE_DEVSSD1331)
+#if (WARP_BUILD_ENABLE_DEVSSD1331)
 	#include "devSSD1331.h"
 //	volatile WarpSPIDeviceState			deviceSSD1331State;
-//#endif
+#endif
 
 #if (WARP_BUILD_ENABLE_DEVLPS25H)
 	#include "devLPS25H.h"
@@ -1716,11 +1716,9 @@ main(void)
 	 *	Initialization: Devices hanging off SPI
 	 */
 
-	//#if (WARP_BUILD_ENABLE_DEVSSD1331)
-		warpPrint("About to devSSD1331init()... ");
+	#if (WARP_BUILD_ENABLE_DEVSSD1331)
 		devSSD1331init();
-		warpPrint("done.\n");
-	//#endif
+	#endif
 
 	#if (WARP_BUILD_ENABLE_DEVADXL362)
 		/*
