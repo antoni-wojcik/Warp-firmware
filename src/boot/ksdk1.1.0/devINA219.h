ce5b477fa0d1dd6931aa98c9b5d5e8b45869d460
Antoni <antekwojcik2@gmail.com>
2023-02-18 19:03:22 +0000

    Fix argument types in function declarations

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -2,6 +2,6 @@
 
 void		initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
 WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
-WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister, uint8_t payloadBtye);
-WarpStatus	configureSensorINA219(uint8_t payloadF_SETUP, uint8_t payloadCTRL_REG1);
+WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payloadBtye);
+WarpStatus	configureSensorINA219(uint16_t payloadCONF, uint16_t payloadCAL);
 void		printSensorDataINA219(bool hexModeFlag);
