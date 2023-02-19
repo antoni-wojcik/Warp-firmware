0dc91f3781b12d25b462cee46686d450b8a013e7
Antoni <antekwojcik2@gmail.com>
2023-02-19 15:07:06 +0000

    Fix voltage readings; Add current and resistance readings

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -101,6 +101,8 @@ configureSensorINA219(uint16_t payloadCONF, uint16_t payloadCAL)
 							payloadCAL /* payload: Calibrate the sensor for current measuremnts */
 							);
 
+	warpPrint("\r\n\tINA219 INITIALIZATION: %d, %d\n", i2cWriteStatus1, i2cWriteStatus2);
+
 	return (i2cWriteStatus1 | i2cWriteStatus2);
 }
 
@@ -154,61 +156,170 @@ readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes)
 	return kWarpStatusOK;
 }
 
+static WarpStatus
+tryI2CSlaveReadSensorRegister(uint8_t i2cAddress)
+{
+	uint8_t		commandByte[1] = {0x00};
+	i2c_status_t	status;
+
+	i2c_device_t slave =
+	{
+		.address = i2cAddress,
+		.baudRate_kbps = gWarpI2cBaudRateKbps
+	};
+
+	warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+	warpEnableI2Cpins();
+
+	status = I2C_DRV_MasterReceiveDataBlocking(
+							0 /* I2C peripheral instance */,
+							&slave,
+							commandByte,
+							1,
+							(uint8_t *)deviceINA219State.i2cBuffer,
+							2,
+							gWarpI2cTimeoutMilliseconds);
+
+	if (status != kStatus_I2C_Success)
+	{
+		return kWarpStatusDeviceCommunicationFailed;
+	}
+
+	return kWarpStatusOK;
+}
+
 void
-printSensorDataINA219(bool hexModeFlag)
+printAllSensorDataINA219(bool hexModeFlag)
 {
 	uint16_t	readSensorRegisterValueLSB;
 	uint16_t	readSensorRegisterValueMSB;
-	int16_t	readSensorRegisterValueCombined;
+	int16_t		readSensorRegisterValueCombined;
 	WarpStatus	i2cReadStatus;
+
+	int16_t 	shuntVoltage;
+	uint16_t	busVoltage;
+	int16_t 	current;
 
 
 	warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
 
 
-    /* Read the shunt voltage. In the standard setting, the shunt voltage LSB is 0.01 mV */
+    /* Read the shunt voltage. With the With the CONFIGURATION REGISTER set to 0x399F, the shunt voltage LSB is 0.01 mV */
 	i2cReadStatus = readSensorRegisterINA219(kWarpSensorOutputRegisterINA219_SHUNT, 2 /* numberOfBytes */);
 	readSensorRegisterValueMSB = deviceINA219State.i2cBuffer[0];
 	readSensorRegisterValueLSB = deviceINA219State.i2cBuffer[1];
 	readSensorRegisterValueCombined = ((readSensorRegisterValueMSB & 0xFF) << 8) | readSensorRegisterValueLSB;
 
-    int16_t shuntVoltage = readSensorRegisterValueCombined; /* 1 bit for the sign, 15 bits for the */
+	if (hexModeFlag)
+	{
+		warpPrint(" 0x%02x 0x%02x,", readSensorRegisterValueMSB, readSensorRegisterValueLSB);
+	}
+
+    shuntVoltage = readSensorRegisterValueCombined; 
 
     /* Read the bus voltage. The bus voltage LSB is always 4mV */
-    i2cReadStatus = readSensorRegisterINA219(kWarpSensorOutputRegisterINA219_SHUNT, 2 /* numberOfBytes */);
+    i2cReadStatus = readSensorRegisterINA219(kWarpSensorOutputRegisterINA219_BUS, 2 /* numberOfBytes */);
+	readSensorRegisterValueMSB = deviceINA219State.i2cBuffer[0];
+	readSensorRegisterValueLSB = deviceINA219State.i2cBuffer[1];
+	readSensorRegisterValueCombined = ((readSensorRegisterValueMSB & 0xFF) << 8) | readSensorRegisterValueLSB;\
+
+	if (hexModeFlag)
+	{
+		warpPrint(" 0x%02x 0x%02x,", readSensorRegisterValueMSB, readSensorRegisterValueLSB);
+	}
+
+    bool busVoltageOverflow = readSensorRegisterValueLSB & 0x01;
+//  bool convReadyBit = readSensorRegisterValueLSB & 0x02;
+    busVoltage = readSensorRegisterValueCombined >> 3;
+
+	/* Read the current. With the CALIBRATION REGISTER set to 0x4FFF, the current LSB is 20 uA */
+	i2cReadStatus = readSensorRegisterINA219(kWarpSensorOutputRegisterINA219_CURRENT, 2 /* numberOfBytes */);
 	readSensorRegisterValueMSB = deviceINA219State.i2cBuffer[0];
 	readSensorRegisterValueLSB = deviceINA219State.i2cBuffer[1];
 	readSensorRegisterValueCombined = ((readSensorRegisterValueMSB & 0xFF) << 8) | readSensorRegisterValueLSB;
 
-    bool busVoltageOverflow = readSensorRegisterValueLSB & 0x01;
-//  bool convReadyBit = readSensorRegisterValueLSB & 0x02;
-    int16_t busVoltage = readSensorRegisterValueCombined >> 3;
+	if (hexModeFlag)
+	{
+		warpPrint(" 0x%02x 0x%02x,", readSensorRegisterValueMSB, readSensorRegisterValueLSB);
+	}
 
-	/*
-	 *	Sign extend the 14-bit value based on knowledge that upper 2 bit are 0:
-	 */
-	// readSensorRegisterValueCombined = (readSensorRegisterValueCombined ^ (1 << 13)) - (1 << 13);
+    current = readSensorRegisterValueCombined;
+
 
 	if (i2cReadStatus != kWarpStatusOK)
 	{
-		warpPrint(" ----, ----,");
+		warpPrint(" ----, ----, ----, ----,");
 	}
 	else
 	{
-		if (hexModeFlag)
+		if (!hexModeFlag)
 		{
-			warpPrint(" 0x%02x 0x%02x,", readSensorRegisterValueMSB, readSensorRegisterValueLSB);
-		}
-		else
-		{
+			int16_t resistanceInt = (int16_t)((float)(busVoltage) / (float)(current) * 200.0f); /* times 200 because of the factors from LSBs */
+
             if (busVoltageOverflow)
             {
-                warpPrint(" BUS VOLTAGE OVERFLOW, %d, %d,", shuntVoltage, busVoltage);
+                warpPrint(" %d, (BUS VOLTAGE OVERFLOW) %u, %d, %d,", shuntVoltage, busVoltage, current, resistanceInt);
             }
             else
             {
-			    warpPrint(" %d, %d,", shuntVoltage, busVoltage);
+			    warpPrint(" %d, %u, %d, %d,", shuntVoltage, busVoltage, current, resistanceInt);
             }
+		}
+	}
+}
+
+void
+printCurrentMicroamperesINA219()
+{
+	uint16_t	readSensorRegisterValueLSB;
+	uint16_t	readSensorRegisterValueMSB;
+	int16_t		readSensorRegisterValueCombined;
+	WarpStatus	i2cReadStatus;
+
+	int16_t 	currentMicroamperes;
+
+
+	warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+
+
+	/* Read the current. With the CALIBRATION REGISTER set to 0x4FFF, the current LSB is 20 uA */
+	i2cReadStatus = readSensorRegisterINA219(kWarpSensorOutputRegisterINA219_CURRENT, 2 /* numberOfBytes */);
+	readSensorRegisterValueMSB = deviceINA219State.i2cBuffer[0];
+	readSensorRegisterValueLSB = deviceINA219State.i2cBuffer[1];
+	readSensorRegisterValueCombined = ((readSensorRegisterValueMSB & 0xFF) << 8) | readSensorRegisterValueLSB;
+
+	currentMicroamperes = readSensorRegisterValueCombined * 20; // raw data * 20 uA
+
+
+	if (i2cReadStatus != kWarpStatusOK)
+	{
+		warpPrint(" ----,");
+	}
+	else
+	{
+		warpPrint(" %d,", currentMicroamperes);
+	}
+}
+
+/* Utility to find all detected I2C slave addresses and list them */
+void
+searchOverI2CSlaveAddresses() {
+	for (uint8_t i = 0x00; i <= 0x7F; i++) {
+		WarpStatus status = tryI2CSlaveReadSensorRegister(i);
+
+		if(!status)
+		{
+			warpPrint("\rI2C slave address 0x%02X found\n", i);
+
+			/* Print all sensor data if sensor detected */
+			/*
+			if(i == 0x40)
+			{
+				warpPrint("\r");
+				printAllSensorDataINA219(false);
+				warpPrint("\n);
+			}
+			*/
 		}
 	}
 }
